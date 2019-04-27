6e99948320c1fb0a798a894f248d2f4704c5e5af
Javier Agüera <javibm6@gmail.com>
2019-04-27 13:30:02 +0200

    + Singleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace LD44
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        [SerializeField]
+        private int initialChamacos;
+
+        private int currentChamacos;
+        public int CurrentChamacos {
+            get {
+                return currentChamacos;
+            }    
+        }
+
+        private void Awake() {
+            currentChamacos = initialChamacos;
+        }
+
+
+    }
+}
