197001c07b4956ac9eba9ac42ee98eae55350e12
Javier Agüera <javibm6@gmail.com>
2019-04-27 15:03:37 +0200

    + Muchos managers absurdos que ni se porque los he hecho singleton

---
+++ Assets/Scripts/AlienManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LD44
+{
+    public class AlienManager : Singleton<AlienManager>
+    {
+        private int currentEnergyNeeded;
+        public int CurrentEnergyNeeded
+        {
+            get
+            {
+                return currentEnergyNeeded;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace LD44
+{
+    public class TimeManager : Singleton<TimeManager>
+    {
+        private float timePerDay;
+
+        public Action OnDayEnded;
+        public Action OnFactoryTicked;
+
+        private float currentTime;
+
+        private bool initialized = false;
+
+        private float factoryTickedTime = 1.0f;
+
+        public void Init(float _timePerDay)
+        {
+            initialized = true;
+            timePerDay = _timePerDay;
+            currentTime = 0.0f;
+        }
+
+        private void Update()
+        {
+            if (initialized)
+            {
+                if (currentTime > timePerDay)
+                {
+                    currentTime = 0.0f;
+                    if (OnDayEnded != null)
+                    {
+                        OnDayEnded();
+                    }
+                }
+                if (currentTime > factoryTickedTime)
+                {
+                    if (OnFactoryTicked != null)
+                    {
+                        OnFactoryTicked();
+                    }
+                }
+
+                currentTime += Time.deltaTime;
+            }
+        }
+    }
+}
